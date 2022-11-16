ccac17febb39fa25d2446882bc3c23768b33c288
ProgSume <118303612+ProgSume@users.noreply.github.com>
2022-11-15 22:43:38 -0300

    .

---
+++ cadastro.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Cadastre-se</title>
+</head>
+<body>
+    <header id="cadastro-title">
+        <h1>Crie sua conta</h1>
+    </header>
+    <div class="cadastro">
+       <form action="">
+       <div>
+          <label for="name">Name</label> <br>
+          <input type="text" name="password" id="password" placeholder="Name"><br>
+        </div>
+        <div>
+          <label for="email">Email</label> <br>
+          <input type="email" name="email" id="email" placeholder="E-mail Address">
+        </div>
+        <div>
+          <label for="password">Password</label> <br>
+          <input type="password" name="password" id="password" placeholder="Password"><br>
+        </div>
+        <div>
+          <label for="password">Confirm Password</label> <br>
+          <input type="password" name="password" id="password" placeholder="Password"><br>
+        </div>
+        <div>
+          <input type="submit" value="Criar" id="btn"> <br>
+        </div>
+       </form>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Harpa Cifra</title>
+</head>
+<body>
+    <header id="first-title">
+        <h1>Harpa Cifrada</h1>
+    </header>
+    
+    <section class="text-comented">
+      <p>Essa plataforma foi desenvolvida com o intuito de facilitar buscas e aprendizado, com o <strong>harpa Cifra</strong> você pode encontrar seus hinos favoritos e toca-los</p>
+    </section>
+
+    <div class="form">
+       <form action="">
+        <div>
+          <label for="email">Email</label> <br>
+          <input type="email" name="email" id="email" placeholder="E-mail Address">
+        </div>
+        <div>
+          <label for="password">Password</label> <br>
+          <input type="password" name="password" id="password" placeholder="Password"><br>
+        </div>
+        <div>
+          <input type="submit" value="Entrar" id="btn"> <br>
+          <a href="./cadastro.php">Cadastrar</a>
+        </div>
+       </form>
+    </div>
+</body>
+</html>
